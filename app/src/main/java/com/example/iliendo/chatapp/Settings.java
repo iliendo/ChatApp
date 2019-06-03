3e165fb0ac42473f99a637696c087b71fa50f31b
Ilias Azagagh <ilias.azagagh@hva.nl>
2019-06-03 14:40:02 +0200

    Revert "Made fragments into innerclasses"
    This reverts commit b251176e66840d617f5f99f2bed8fa1e121756ee.

--- app/src/main/java/com/example/iliendo/chatapp/Settings.java
+++ app/src/main/java/com/example/iliendo/chatapp/Settings.java
@@ -1,9 +1,6 @@
 package com.example.iliendo.chatapp;
 
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -11,18 +8,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-
-import com.firebase.client.Firebase;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,26 +19,10 @@ public class Settings extends AppCompatActivity {
     private SectionsPagerAdapter mSectionsPagerAdapter;
     private ViewPager mViewPager;
 
-    public static final int READ_EXTERNAL_STORAGE = 0;
-    private static final int GALLERY_INTENT = 0;
-    private Firebase mRoofRef;
-    private Uri mImageUri;
-    private DatabaseReference mDatabaseRef;
-    private StorageReference mStorageRef;
-
-    private Handler mHandler = new Handler();
-
-    private FirebaseAuth mAuth;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
-        Firebase.setAndroidContext(this);
-        mAuth = FirebaseAuth.getInstance();
-        final String name = mAuth.getCurrentUser().getDisplayName();
-        final String email = mAuth.getCurrentUser().getEmail();
 
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
@@ -106,63 +75,4 @@ public class Settings extends AppCompatActivity {
             return mFragmentTitles.get(position);
         }
     }
-
-    public static class Tab1Fragment extends Fragment {
-        private static final String TAG = "Tab1Fragment";
-
-        private EditText mEmail;
-        private EditText mOldPassword;
-        private EditText mNewPassword;
-        private EditText mRepeatNewPassword;
-        private Button mSubmit;
-
-        @Nullable
-        @Override
-        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            View view = inflater.inflate(R.layout.tab1_fragment, container, false);
-
-            mEmail = view.findViewById(R.id.et_email);
-            mOldPassword = view.findViewById(R.id.et_old_password);
-            mNewPassword = view.findViewById(R.id.et_new_password);
-            mRepeatNewPassword = view.findViewById(R.id.et_repeat_new_password);
-            mSubmit = view.findViewById(R.id.btn_submit);
-
-            mSubmit.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    // TODO: check text and submit to firebase
-                }
-            });
-            return view;
-
-        }
-    }
-
-
-    public static class Tab2Fragment extends Fragment {
-        private static final String TAG = "Tab2Fragment";
-
-        private ImageView mImageView;
-        private ProgressBar mProgressBar;
-
-        @Nullable
-        @Override
-        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            View view = inflater.inflate(R.layout.tab2_fragment, container, false);
-
-            mImageView = view.findViewById(R.id.iv_profile);
-            mProgressBar = view.findViewById(R.id.pb_progressbar);
-
-            // Tapping the image opens the gallery
-            mImageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-
-                }
-            });
-
-            return view;
-        }
-    }
 }
-
